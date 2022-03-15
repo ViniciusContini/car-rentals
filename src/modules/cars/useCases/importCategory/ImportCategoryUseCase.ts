f39b16e89869ac44705baf9857ce9c9101853df4
ViniciusContini <vinicius.contini@hotmail.com>
2022-03-15 07:36:41 -0300

    creating file reading

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,16 @@
+import { parse } from "csv-parse";
+import fs from "fs";
+
 class ImportCategoryUseCase {
   execute(file: Express.Multer.File) {
-    console.log(file);
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = parse();
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
