89cbd1944f0a31967e31cfb20713e739dd2071ce
ViniciusContini <vinicius.contini@hotmail.com>
2022-03-18 07:52:28 -0300

    encapsulating application in docker containers

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -12,6 +12,7 @@ const upload = multer({
 });
 
 categoriesRoutes.post("/", (request, response) => {
+  console.log("Reload is working!");
   return createCategoryController.handle(request, response);
 });
 
