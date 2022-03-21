4d3c0bd09eecdafb69ec65b2e432ac63265334ba
ViniciusContini <vinicius.contini@hotmail.com>
2022-03-21 20:19:59 -0300

    Create categories migration

---
+++ src/database/migrations/1647902256462-CreateCategories.ts
@@ -0,0 +1,35 @@
+import { Column, MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCategories1647902256462 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "categories",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("categories");
+  }
+}
