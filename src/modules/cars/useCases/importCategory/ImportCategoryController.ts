6e0c4f3fca57658f3f3e027f884fb63b42cc0bad
ViniciusContini <vinicius.contini@hotmail.com>
2022-04-07 20:00:01 -0300

    feat: :sparkles: Creating users entities and migration

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -9,7 +9,7 @@ class ImportCategoryController {
     const importCategoryUseCase = container.resolve(ImportCategoryUseCase);
 
     await importCategoryUseCase.execute(file);
-    return response.send();
+    return response.status(201).send();
   }
 }
 
