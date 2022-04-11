a40d6a7b3fac7e295f8e6d2112f7ee919e332af8
ViniciusContini <vinicius.contini@hotmail.com>
2022-04-11 19:11:17 -0300

    :truck: Moving the middlewares folder

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { AppError } from "../errors/AppErrors";
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError("Token missing!", 401);
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "939e6ade0a3a74a18988ea5e771480fa"
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError("User does not exits!", 401);
+    }
+
+    next();
+  } catch (error) {
+    throw new AppError("Invalid token!", 401);
+  }
+}
