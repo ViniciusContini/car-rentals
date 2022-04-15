3ccf0008ac01594353ba96dd72db498cb426aa06
ViniciusContini <vinicius.contini@hotmail.com>
2022-04-15 09:14:05 -0300

    :rocket: Update user avatar and delete the older avatar image

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import multer from "multer";
 
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const usersRoutes = Router();
-const uploadAvatar = multer({
-  dest: "./avatar",
-});
+
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
@@ -16,7 +17,8 @@ usersRoutes.post("/", createUserController.handle);
 
 usersRoutes.patch(
   "/avatar",
-  uploadAvatar.single("file"),
+  ensureAuthenticated,
+  uploadAvatar.single("avatar"),
   updateUserAvatarController.handle
 );
 
